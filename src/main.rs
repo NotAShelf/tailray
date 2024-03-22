cf345c07514b3695eeded1a7f0053c44c4954845
NotAShelf <raf@notashelf.dev>
2024-03-22 10:17:11 +0300

    docs: explain `park()` loop

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod tailscale;
 mod tray;
 
 use ksni::TrayService;
+use std::thread::park;
 
 fn main() {
     // initialize logger
@@ -14,7 +15,11 @@ fn main() {
     TrayService::new(crate::tray::menu::SysTray::new()).spawn();
 
     // keep the main thread alive
+    // NOTE: The documentation for park reads:
+    // "A call to park does not guarantee that the thread will
+    // remain parked forever, and callers should be prepared for this possibility."
+    // hence the loop
     loop {
-        std::thread::park();
+        park();
     }
 }
