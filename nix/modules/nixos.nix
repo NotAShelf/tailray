4fd5c8b3e4a8296454938d48f5e20e4d20c4c7af
mBuschauer <marco@buschauer.us>
2026-01-18 16:17:59 -0500

    cleanup

--- nix/modules/nixos.nix
+++ nix/modules/nixos.nix
@@ -1,9 +1,6 @@
-self: {
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
+self:
+{ config, pkgs, lib, ... }:
+let
   inherit (lib.options) mkEnableOption mkPackageOption mkOption;
   inherit (lib.meta) getExe;
   inherit (lib.types) nullOr str bool;
@@ -11,16 +8,14 @@ self: {
 
   cfg = config.services.tailray;
 in {
-  meta.maintainers = with lib.maintainers; [NotAShelf];
+  meta.maintainers = with lib.maintainers; [ NotAShelf ];
 
   options.services.tailray = {
     enable = mkEnableOption "Tailray, a Tailscale tray";
 
-    package =
-      mkPackageOption pkgs "tailray" {}
-      // {
-        default = self.packages.${pkgs.stdenv.hostPlatform.system}.tailray;
-      };
+    package = mkPackageOption pkgs "tailray" { } // {
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.tailray;
+    };
 
     adminUrl = mkOption {
       description = "The URL the Admin Console button should point to";
@@ -38,18 +33,21 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [cfg.package];
+    environment.systemPackages = [ cfg.package ];
 
     systemd.services.tailray = {
       serviceConfig = {
-        WantedBy = ["multi-user.target"];
+        WantedBy = [ "multi-user.target" ];
         ExecStart = "${getExe cfg.package}";
         Restart = "always";
         RestartSec = "10";
       };
 
-      environment.TAILRAY_ADMIN_URL = mkIf (cfg.adminUrl != null) cfg.adminUrl;
-      environment.TAILRAY_THEME = if cfg.darkMode then "dark" else "light";
+      environment = {
+        TAILRAY_ADMIN_URL = mkIf (cfg.adminUrl != null) cfg.adminUrl;
+        TAILRAY_THEME = if cfg.darkMode then "dark" else "light";
+      };
+      
     };
   };
 }
